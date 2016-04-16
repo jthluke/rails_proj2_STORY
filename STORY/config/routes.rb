4c5be8804e5307431110251b2a1e9a6155ed6166
Iris Chen <irichen@berkeley.edu>
2016-04-15 20:30:05 -0700

    fixed routes.rb oops

--- STORY/config/routes.rb
+++ STORY/config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :posts
-  resources :stories
-  resources :links do
+  resources :stories do
       member do
-        put "like", to: "links#upvote"
-        put "dislike", to: "links#downvote"
+        put "like", to: "stories#upvote"
+        put "dislike", to: "stories#downvote"
+      end
+  end
+  resources :posts do
+      member do
+        put "like", to: "posts#upvote"
+        put "dislike", to: "posts#downvote"
       end
   end
 end
