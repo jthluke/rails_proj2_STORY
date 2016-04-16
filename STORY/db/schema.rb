34d67e06c268ff41e22477acc8c505dc63a866c3
leeren <leeren@berkeley.edu>
2016-04-15 20:03:17 -0700

    added story id

--- STORY/db/schema.rb
+++ STORY/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416023550) do
+ActiveRecord::Schema.define(version: 20160416030259) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "vote"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160416023550) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "story_id"
   end
 
   create_table "stories", force: :cascade do |t|
