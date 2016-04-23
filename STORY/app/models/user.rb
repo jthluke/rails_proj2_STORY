5fcdca99b36f85da69e80e98a038e6b407a91f91
Iris Chen <irichen@berkeley.edu>
2016-04-23 03:16:20 -0700

    Added STYLEgit statusgit status We never go out of style~

--- STORY/app/models/user.rb
+++ STORY/app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_many :stories
   has_many :posts
   acts_as_voter
-  has_attached_file :photo, styles: { medium: "200x200>", thumb: "50x50>" }, default_url: "https://cdn.thinglink.me/api/image/727110550026190849/1240/10/scaletowidth"
+  has_attached_file :photo, default_url: "https://cdn.thinglink.me/api/image/727110550026190849/1240/10/scaletowidth"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
   validates_presence_of :username
   validates_uniqueness_of :username
