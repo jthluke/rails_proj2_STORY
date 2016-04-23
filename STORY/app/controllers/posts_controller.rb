fa4dbffe10428f6dcb04efeac6110fbcaeaca46d
Ian Lee <ian.lee@berkeley.edu>
2016-04-23 15:20:21 -0700

    added points and modified collapses

--- STORY/app/controllers/posts_controller.rb
+++ STORY/app/controllers/posts_controller.rb
@@ -8,7 +8,8 @@ class PostsController < ApplicationController
 	def create
 		@post = Post.create post_params
 		@post.user_id = current_user.id
-        current_user.points += 1
+    current_user.points += 1
+		current_user.save
 		@post.content = post_params[:content]
 		@post.success = false
 		@post.vote = 0
@@ -24,21 +25,25 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     user = User.find(@post.user_id)
     user.points += 1
+		current_user.points += 1
+		current_user.save
     @post.liked_by current_user
 		if @post.score == 1 && @post.success == false
 			@post.success = true
 			@post.save
 			story = Story.find(@post.story_id)
-            user.points += 5
+      user.points += 5
 			# old = story.posts
 			# story.posts = []
 			story.posts.each do |p|
 				if p.success == false
 
 					story.posts.delete(p)
+					p.destroy
 				end
 			end
 		end
+		user.save
 		redirect_to story_url(id:@post.story.id)
   end
 
@@ -46,20 +51,34 @@ class PostsController < ApplicationController
       @post = Post.find(params[:id])
       user = User.find(@post.user_id)
       user.points -= 1
+			user.save
       @post.disliked_by current_user
+			if @post.score == -1 && @post.success == false
+				story = Story.find(@post.story_id)
+				story.posts.delete(@post)
+				@post.destroy
+			end
       redirect_to story_url(id:@post.story_id)
     end
 
 	  def unupvote
-          @post = Post.find(params[:id])
-	      @post.unliked_by current_user
-          redirect_to story_url(id:@post.story_id)
+      @post = Post.find(params[:id])
+      @post.unliked_by current_user
+      redirect_to story_url(id:@post.story_id)
+			user = User.find(@post.user_id)
+      user.points -= 1
+			user.save
+			current_user.points -= 1
+			current_user.save
 	  end
 
 	  def undownvote
-          @post = Post.find(params[:id])
-	    @post.undisliked_by current_user
-          redirect_to story_url(id:@post.story_id)
+      @post = Post.find(params[:id])
+    	@post.undisliked_by current_user
+      redirect_to story_url(id:@post.story_id)
+			user = User.find(@post.user_id)
+      user.points += 1
+			user.save
 	  end
 
 
